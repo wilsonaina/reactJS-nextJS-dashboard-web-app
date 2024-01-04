12e481bd23b36212ceaab98f61e853a08689b8b4
Wilson Aina <wilsonaina@hotmail.com>
2024-01-04 14:49:24 -0500

    react_js next_js files for dashboard project

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <>This is the invoices page! Baby!</>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+import { NextFont } from "next/dist/compiled/@next/font";
+
+export const inter: NextFont = Inter({
+    subsets: ['latin']
+});
+
+export const lusitana: NextFont = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
